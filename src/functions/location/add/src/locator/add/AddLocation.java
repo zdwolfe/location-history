c591d3df39f9697c7a5958a3daa78fcbba1f9c26
Zach Wolfe <wolfe.zach@gmail.com>
2016-10-31 21:25:43 -0700

    Module and apex setup

---
+++ src/functions/location/add/src/locator/add/AddLocation.java
@@ -0,0 +1,48 @@
+package locator.add;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class AddLocation {
+
+    public static class AddLocationRequest {
+        String hello;
+
+        public String getHello() {
+            return hello;
+        }
+
+        public void setHello(String hello) {
+            this.hello = hello;
+        }
+
+        public AddLocationRequest(String hello) {
+            this.hello = hello;
+        }
+
+        public AddLocationRequest() {
+        }
+    }
+
+    public static class AddLocationResponse {
+        String hello;
+
+        public String getHello() {
+            return hello;
+        }
+
+        public void setHello(String hello) {
+            this.hello = hello;
+        }
+
+        public AddLocationResponse(String hello) {
+            this.hello = hello;
+        }
+
+        public AddLocationResponse() {
+        }
+    }
+
+    public AddLocationResponse handler(AddLocationRequest event, Context context) {
+        return new AddLocationResponse(event.getHello());
+    }
+}
